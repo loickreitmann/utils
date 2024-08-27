ecdf23028b61a34115dda2a1533e4cc6490be3fb
Loïc Kreitmann <loickreitmann@gmail.com>
2024-08-26 18:17:42 -0700

    added test case for negative integer value

--- random_string_test.go
+++ random_string_test.go
@@ -1,13 +1,14 @@
 package utils
 
 import (
+	"math"
 	"testing"
 )
 
 func TestUtils_RandomString(t *testing.T) {
 	// arrange
 	var testUtils Utils
-	expectedLens := []int{1, 5, 7, 22, 36, 789, 10000}
+	expectedLens := []int{-23, 0, 1, 5, 7, 22, 36, 789, 10000}
 	actualLens := []int{}
 
 	// act
@@ -18,7 +19,7 @@ func TestUtils_RandomString(t *testing.T) {
 
 	// assert
 	for i := range actualLens {
-		if actualLens[i] != expectedLens[i] {
+		if actualLens[i] != int(math.Abs(float64(expectedLens[i]))) {
 			t.Errorf("wrong string length; expected %d, got %d", expectedLens[i], actualLens[i])
 		}
 	}
