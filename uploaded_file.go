c5c5329a883bca000667d3138cab9b0b4894cd45
Loïc Kreitmann <loickreitmann@gmail.com>
2024-08-26 19:02:57 -0700

    struct to hold uploaded file information

---
+++ uploaded_file.go
@@ -0,0 +1,8 @@
+package utils
+
+// UplaodedFile is a struct storing the information about an uploaded file.
+type UplaodedFile struct {
+	NewFilename      string
+	OriginalFilename string
+	FileSize         int64
+}
