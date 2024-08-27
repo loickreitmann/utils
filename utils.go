8c433f45ea2c37aa1e063bf952bfc542d2bf9773
Loïc Kreitmann <loickreitmann@gmail.com>
2024-08-26 18:25:26 -0700

    splitting into individual file for separation of concern

--- utils.go
+++ utils.go
@@ -2,31 +2,6 @@
 // Inspired by Trevor Sawler's "Building a Module in Go." Udemy course.
 package utils
 
-import "crypto/rand"
-
-var sourceCharacters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_+!@#$%*")
-
 // Utils is the type used to instantiate the module. Any variable of this type will have access to all
 // the methods with the *Utils receiver.
 type Utils struct{}
-
-// RandomString generates and returns a string of a specified length of random characters.
-// The characters are randomnly asembled from the source of the following posible characters:
-// abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_+!@#$%*
-func (o *Utils) RandomString(length int) string {
-	// Going for performance over readability. Using the below if statement directly operates on
-	// integers without any additional function calls or type conversions.
-	// Could have used:
-	//  length = int(math.Abs(float64(length)))
-	// but it would
-	if length < 0 {
-		length = -length
-	}
-	randomString := make([]rune, length)
-	for i := range randomString {
-		primeNum, _ := rand.Prime(rand.Reader, len(sourceCharacters))
-		x, y := primeNum.Uint64(), uint64(len(sourceCharacters))
-		randomString[i] = sourceCharacters[x%y]
-	}
-	return string(randomString)
-}
