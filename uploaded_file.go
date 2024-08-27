b1696356d5a432733d8c414e6a2803367efa3ded
Loïc Kreitmann <loickreitmann@gmail.com>
2024-08-27 00:21:05 -0700

    fixed a typo in the UploadedFile struct's name

--- uploaded_file.go
+++ uploaded_file.go
@@ -1,7 +1,7 @@
 package utils
 
-// UplaodedFile is a struct storing the information about an uploaded file.
-type UplaodedFile struct {
+// UploadedFile is a struct storing the information about an uploaded file.
+type UploadedFile struct {
 	NewFilename      string
 	OriginalFilename string
 	FileSize         int64
