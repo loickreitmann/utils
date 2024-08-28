8481ae3c2fb31dcc02073b1f0b42a33e227eeb70
Loïc Kreitmann <loickreitmann@gmail.com>
2024-08-27 21:45:25 -0700

    updated UpoadFiles test to test multiple file uploads

--- upload_files_test.go
+++ upload_files_test.go
@@ -3,11 +3,14 @@ package utils
 import (
 	"fmt"
 	"image"
+	"image/gif"
+	"image/jpeg"
 	"image/png"
 	"io"
 	"mime/multipart"
 	"net/http/httptest"
 	"os"
+	"path/filepath"
 	"sync"
 	"testing"
 )
@@ -16,35 +19,35 @@ import (
 var uploadTests = []struct {
 	name          string
 	allowedTypes  []string
-	testFile      string
+	testFiles     []string
 	renameFiles   bool
 	errorExpected bool
 }{
 	{
 		name:          "allowed, not renamed",
 		allowedTypes:  []string{"image/jpeg", "image/png", "image/gif"},
-		testFile:      "./testdata/upload_test.png",
+		testFiles:     []string{"./testdata/upload_test.jpg", "./testdata/upload_test.gif", "./testdata/upload_test.png"},
 		renameFiles:   false,
 		errorExpected: false,
 	},
 	{
 		name:          "allowed, renamed",
 		allowedTypes:  []string{"image/jpeg", "image/png", "image/gif"},
-		testFile:      "./testdata/upload_test.png",
+		testFiles:     []string{"./testdata/upload_test.gif", "./testdata/upload_test.jpg", "./testdata/upload_test.png"},
 		renameFiles:   true,
 		errorExpected: false,
 	},
 	{
 		name:          "not allowed",
 		allowedTypes:  []string{"image/jpeg", "image/gif"},
-		testFile:      "./testdata/upload_test.png",
+		testFiles:     []string{"./testdata/upload_test.png"},
 		renameFiles:   false,
 		errorExpected: true,
 	},
 	{
 		name:          "all types allowed, not renamed",
 		allowedTypes:  []string{},
-		testFile:      "./testdata/upload_test.png",
+		testFiles:     []string{"./testdata/upload_test.png", "./testdata/upload_test.gif", "./testdata/upload_test.jpg"},
 		renameFiles:   false,
 		errorExpected: false,
 	},
@@ -52,64 +55,77 @@ var uploadTests = []struct {
 
 func TestUtils_UploadFiles(t *testing.T) {
 	wg := sync.WaitGroup{}
-	for _, e := range uploadTests {
-		// ARRANGNE
-		// set up a pipe to avoid buffering
-		pr, pw := io.Pipe()
-		writer := multipart.NewWriter(pw)
-		wg.Add(1)
+	for _, ut := range uploadTests {
+		for _, testFile := range ut.testFiles {
+			// ARRANGE
+			// set up a pipe to avoid buffering
+			pr, pw := io.Pipe()
+			writer := multipart.NewWriter(pw)
+			wg.Add(1)
 
-		go func() {
-			defer writer.Close()
-			defer wg.Done()
+			go func() {
+				defer writer.Close()
+				defer wg.Done()
 
-			// create the form data field 'file'
-			part, err := writer.CreateFormFile("file", e.testFile)
+				// create the form data field 'file'
+				part, err := writer.CreateFormFile("file", testFile)
+				if err != nil {
+					t.Error(err)
+				}
+				file, err := os.Open(testFile)
+				if err != nil {
+					t.Error(err)
+				}
+				defer file.Close()
+
+				img, _, err := image.Decode(file)
+				if err != nil {
+					t.Error("error decoding image", err)
+				}
+				ext := filepath.Ext(testFile)
+				switch ext {
+				case ".png":
+					if err = png.Encode(part, img); err != nil {
+						t.Error(err)
+					}
+				case ".gif":
+					if err = gif.Encode(part, img, nil); err != nil {
+						t.Error(err)
+					}
+				case ".jpg":
+					if err = jpeg.Encode(part, img, nil); err != nil {
+						t.Error(err)
+					}
+				}
+			}()
+
+			// read fro the pipe which receives data
+			request := httptest.NewRequest("POST", "/", pr)
+			request.Header.Add("Content-Type", writer.FormDataContentType())
+
+			// ACT
+			testUtils := New()
+			testUtils.AllowedTypes = ut.allowedTypes
+
+			uploadedFiles, err := testUtils.UploadFiles(request, "./testdata/uploads/", ut.renameFiles)
 			if err != nil {
 				t.Error(err)
 			}
-			file, err := os.Open(e.testFile)
-			if err != nil {
-				t.Error(err)
+
+			// ASSERT
+			// Positive test: should not result in an error
+			if !ut.errorExpected {
+				if err != nil {
+					t.Errorf("[%s] error unexpected: %s", ut.name, err.Error())
+				}
+				expectedFile := fmt.Sprintf("./testdata/uploads/%s", uploadedFiles[0].NewFilename)
+				if _, fileErr := os.Stat(expectedFile); os.IsNotExist(fileErr) {
+					t.Errorf("[%s] expected file to exist: %s", ut.name, fileErr.Error())
+				}
+				// clean up
+				_ = os.Remove(expectedFile)
 			}
-			defer file.Close()
-
-			img, _, err := image.Decode(file)
-			if err != nil {
-				t.Error("error decoding image", err)
-			}
-
-			if err = png.Encode(part, img); err != nil {
-				t.Error(err)
-			}
-		}()
-
-		// read fro the pipe which receives data
-		request := httptest.NewRequest("POST", "/", pr)
-		request.Header.Add("Content-Type", writer.FormDataContentType())
-
-		// ACT
-		testUtils := New()
-		testUtils.AllowedTypes = e.allowedTypes
-
-		uploadedFiles, err := testUtils.UploadFiles(request, "./testdata/uploads/", e.renameFiles)
-		if err != nil {
-			t.Error(err)
+			wg.Wait()
 		}
-
-		// ASSERT
-		// Positive test: should not result in an error
-		if !e.errorExpected {
-			if err != nil {
-				t.Errorf("[%s] error unexpected: %s", e.name, err.Error())
-			}
-			expectedFile := fmt.Sprintf("./testdata/uploads/%s", uploadedFiles[0].NewFilename)
-			if _, fileErr := os.Stat(expectedFile); os.IsNotExist(fileErr) {
-				t.Errorf("[%s] expected file to exist: %s", e.name, fileErr.Error())
-			}
-			// clean up
-			_ = os.Remove(expectedFile)
-		}
-		wg.Wait()
 	}
 }
