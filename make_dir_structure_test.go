ff3558a0519a6b89ff22315cc42124526c9cc9c4
Loïc Kreitmann <loickreitmann@gmail.com>
2024-08-28 15:38:45 -0700

    Test of MakeDirStructure for the case when the folder permissions would block the creation

--- make_dir_structure_test.go
+++ make_dir_structure_test.go
@@ -25,6 +25,26 @@ func TestUtils_MakeDirStructure(t *testing.T) {
 		t.Error(err.Error())
 	}
 }
+func TestUtils_MakeDirStructure_Error(t *testing.T) {
+	// ARRANGE
+	var testUtils Utils
+	notPermittedRoot := "./permission_denied_path"
+	// creat a directory with permission to block the creation
+	_ = os.Mkdir(notPermittedRoot, 0555)
+	testPath := notPermittedRoot + "/sub_path"
+	// ACT
+	err := testUtils.MakeDirStructure([]string{testPath})
+	// ASSERT
+	if err == nil {
+		t.Error("error was expected")
+	}
+	// CLEAN UP
+	// get the root path
+	err = os.RemoveAll(notPermittedRoot)
+	if err != nil {
+		t.Error(err.Error())
+	}
+}
 
 func generateRandomPathsSlice() (string, []string) {
 	var paths []string
