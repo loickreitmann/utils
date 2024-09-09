1e8617110a2db52c88fcaaaf5872cd8e2afb94ac
Loïc Kreitmann <loickreitmann@gmail.com>
2024-09-08 18:44:19 -0700

    WriteJSON method

---
+++ write_json.go
@@ -0,0 +1,27 @@
+package utils
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// WriteJSON takes a response status code and arbitrary data and generates and sends json in the http response to the client
+func (u *Utils) WriteJSON(w http.ResponseWriter, status int, data interface{}, headers ...http.Header) error {
+	output, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	if len(headers) > 0 {
+		for key, value := range headers[0] {
+			w.Header().Set(key, value[0])
+		}
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	_, err = w.Write(output)
+	if err != nil {
+		return err
+	}
+	return nil
+}
