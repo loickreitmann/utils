169c041591570b59a9aa469add98f5c591889101
Loïc Kreitmann <loickreitmann@gmail.com>
2024-09-13 00:00:25 -0700

    parseEnvVars private method

--- load_env_vars.go
+++ load_env_vars.go
@@ -1,1 +1,33 @@
 package utils
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func (u *Utils) parseEnvVars(envFile string) error {
+	scanner := bufio.NewScanner(strings.NewReader(envFile))
+	for scanner.Scan() {
+		line := scanner.Text()
+		// ignore enpty lines of comments lines
+		if line == "" || strings.HasPrefix(line, "#") {
+			// move on to the next line
+			continue
+		}
+		// split lines into key-value-pairs
+		kvp := strings.Split(line, "=")
+		if len(kvp) != 2 {
+			// malformed line, ignore it
+			continue
+		}
+		key := strings.TrimSpace(kvp[0])
+		value := strings.TrimSpace(kvp[1])
+		// store the key-value-pairs as OS Env Vars
+		if err := os.Setenv(key, value); err != nil {
+			return fmt.Errorf("error setting %s as en environment variable: %v", key, err)
+		}
+	}
+	return nil
+}
